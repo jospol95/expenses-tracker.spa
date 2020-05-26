11a2d5640e0154cc6094f5f610756e3d48aaee55
JoseP <lanjos26@gmail.com>
2020-05-26 10:03:57 -0400

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteLayoutComponent } from './layout/site-layout/site-layout.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TopNavbarComponent } from './shared/components/top-navbar/top-navbar.component';
+import {MatButtonModule, MatIconModule, MatMenuModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteLayoutComponent,
+    TopNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  exports: [
+    SiteLayoutComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
